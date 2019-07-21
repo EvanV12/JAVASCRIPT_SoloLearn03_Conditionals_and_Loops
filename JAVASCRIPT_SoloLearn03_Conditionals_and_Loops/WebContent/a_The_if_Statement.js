79264a4831621b4860f39f4191928fcb57f5a56e
EvanV12 <44060244+EvanV12@users.noreply.github.com>
2019-07-21 05:19:30 +0300

    Initial commit - "The if Statement"

---
+++ JAVASCRIPT_SoloLearn03_Conditionals_and_Loops/WebContent/a_The_if_Statement.js
@@ -0,0 +1,18 @@
+/*
+if (condition) {
+	statements
+	}
+*/
+
+var myNum1 = 7;
+var myNum2 = 10;
+
+if (myNum1 < myNum2) {
+	alert("JavaScript is easy to learn");
+}
+
+
+if (myNum1 > myNum2) {
+	alert("JavaScript is easy to learn");
+}
+
