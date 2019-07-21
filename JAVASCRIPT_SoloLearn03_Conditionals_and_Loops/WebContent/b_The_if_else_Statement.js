597030fa34a3c5c9fab0f07ebc0c6aaffd6e23a4
EvanV12 <44060244+EvanV12@users.noreply.github.com>
2019-07-21 05:25:58 +0300

    Added "The if else Statement"

---
+++ JAVASCRIPT_SoloLearn03_Conditionals_and_Loops/WebContent/b_The_if_else_Statement.js
@@ -0,0 +1,20 @@
+/*
+if (expression) {
+	// executed if condition is true
+	}
+	else {
+		// executed if condition is false
+		}
+*/
+
+var myNum1 = 7;
+var myNum2 = 10;
+
+if (myNum1 > myNum2) {
+	alert("This is my first condition");
+}
+else {
+	alert("This is my second condition");
+}
+
+// a > b ? alert(a) : alert(b)
