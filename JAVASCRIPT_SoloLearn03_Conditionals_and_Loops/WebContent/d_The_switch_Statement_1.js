eb4738dc1ee281f5244ab2626865167ef4d52c61
EvanV12 <44060244+EvanV12@users.noreply.github.com>
2019-07-23 23:29:14 +0300

    Added "The switch Statement"

---
+++ JAVASCRIPT_SoloLearn03_Conditionals_and_Loops/WebContent/d_The_switch_Statement_1.js
@@ -0,0 +1,29 @@
+/*
+switch (expression) {
+	case n1:
+		statements
+		break;
+	case n2:
+		statements
+		break;
+	default:
+		statements
+	}
+*/
+
+var day = 2;
+
+switch (day) {
+	case 1:
+		document.write("Monday");
+		break;
+	case 2:
+		document.write("Tuesday");
+		break;
+	case 3:
+		document.write("Wednesday");
+		break;
+	default:
+		doxument.write("Another day");
+}
+// Outputs "Tuesday"
